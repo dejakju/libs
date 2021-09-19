0fa415fc34f2f7016a50980a3a180af988faa200
dejakju <dejakju@gmail.com>
2021-09-19 22:51:19 +0200

    Initial commit (needs much more work)

---
+++ Math.cs
@@ -0,0 +1,14 @@
+namespace libs
+{
+    public static class Math
+    {
+        public static double PI = 3.14159265358979323846;
+
+        public static double E = 2.718281828459045;
+
+        public static int Square(int n)
+        {
+            return (n*n);
+        }
+    }
+}
